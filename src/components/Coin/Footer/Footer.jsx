32148cce204a73269b7124a5de01d43075b03ce2
ShubhamGit22 <shubhamchavan258@gmail.com>
2024-07-31 00:45:35 +0530

    .

--- src/components/Coin/Footer/Footer.jsx
+++ src/components/Coin/Footer/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
       </div>
       <div className="footer-center">
         <h2>Get To Know Us</h2>
-       <div className="anchor">
-       <a href="#">Terms Of Service</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">FAQ</a>
-        <a href="#">About Us</a>
-       </div>
+        <div className="anchor">
+          <a href="">Terms Of Service</a>
+          <a href="">Privacy Policy</a>
+          <a href="">FAQ</a>
+          <a href="">About Us</a>
+        </div>
       </div>
       <div className="footer-right">
         <h2> Contact Us </h2>
